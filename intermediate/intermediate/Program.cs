d472f26a37ea913609ef14f72c3a830f68dad2a8
liviafort <livia.fortunato@academico.ifpb.edu.br>
2023-09-25 17:24:39 -0300

    Instance

---
+++ intermediate/intermediate/Program.cs
@@ -0,0 +1,38 @@
+﻿using intermediate;
+using System.Windows;
+
+namespace lecture_1
+{
+    public partial class Lecture1
+    {
+        static void Main(string[] args)
+        {
+            instance_example classe = new instance_example();
+            classe.method_11();
+            
+        }
+
+        //it can be only accessed by the methods under same parent class
+        private static void methode_1(int param1, double param2)
+        {
+            System.Console.WriteLine("method_1");
+        }
+
+        //it can be only accesed from with in the class or any object that inherits off of the class
+        protected static void method_2(int param1, double param2)
+        {
+            System.Console.WriteLine("method_2");
+        }
+
+        public void method_3(int param1, double param2)
+        {
+            System.Console.WriteLine("method_3");
+        }
+
+        //it can be used in another class
+        public static void method_4(int param1, double param2)
+        {
+            System.Console.WriteLine("method_4");
+        }
+    }
+}
---
+++ intermediate/intermediate/instance_example.cs
@@ -0,0 +1,23 @@
+﻿using lecture_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intermediate
+{
+    internal class instance_example
+    {
+        public int method_11()
+        {
+            //call a static
+            Lecture1.method_4(0, 0);
+
+            //call a not static
+            Lecture1 lecture1 = new Lecture1();
+            lecture1.method_3(0, 0);
+            return 0;
+        }
+    }
+}
